3d29ba371d40a6ad7b936a557c9fe819dd0601c1
Kenan Dropić <dropic5@hotmail.com>
2022-06-27 12:28:40 +0200

    add reducers

--- frontend/src/features/globalSlice.tsx
+++ frontend/src/features/globalSlice.tsx
@@ -2,28 +2,56 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface InitialState {
   board: string[][];
+  currentAttempt: {
+    attempt: number;
+    letterPosition: number;
+  };
 }
 
 const initialState: InitialState = {
   board: [
-    ["F", "", "", "", ""],
+    ["", "", "", "", ""],
     ["", "", "", "", ""],
     ["", "", "", "", ""],
     ["", "", "", "", ""],
     ["", "", "", "", ""],
     ["", "", "", "", ""],
   ],
+  currentAttempt: {
+    attempt: 0,
+    letterPosition: 0,
+  },
 };
 
 const globalSlice = createSlice({
   name: "global",
   initialState,
   reducers: {
-    setBoard: (state, action: PayloadAction<string[][]>) => {
-      console.log(action.payload);
-      state.board = action.payload;
+    setBoard: (state, action: PayloadAction<string>) => {
+      const { attempt, letterPosition } = state.currentAttempt;
+      state.board[attempt][letterPosition] = action.payload;
+    },
+    setLetterPosition: (state, action: PayloadAction<string>) => {
+      let operator = action.payload;
+
+      state.currentAttempt = {
+        ...state.currentAttempt,
+        letterPosition:
+          operator === "+"
+            ? state.currentAttempt.letterPosition + 1
+            : state.currentAttempt.letterPosition - 1,
+      };
+    },
+    setAttempt: (state) => {
+      state.currentAttempt = {
+        ...state.currentAttempt,
+        attempt: state.currentAttempt.attempt + 1,
+        letterPosition: 0,
+      };
     },
   },
 });
+
+export const { setBoard, setLetterPosition, setAttempt } = globalSlice.actions;
 
 export default globalSlice.reducer;
--- frontend/src/store.tsx
+++ frontend/src/store.tsx
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "@reduxjs/toolkit";
-import globalReducer from "./features/globalSlce";
+import globalReducer from "./features/globalSlice";
 
 const rootReducer = combineReducers({});
 
