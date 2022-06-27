13d6a8251aa31045f4483a05173815ce3cf2e901
Kenan Dropić <dropic5@hotmail.com>
2022-06-27 20:01:21 +0200

    wordle words

---
+++ frontend/src/components/Words.tsx
@@ -0,0 +1,25 @@
+import words from "../words-data.txt";
+import axios from "axios";
+console.log(words);
+
+export const boardDefault: string[][] = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+
+export const generateWordSet = async () => {
+  let wordSet;
+  let todaysWord;
+  await fetch(words)
+    .then((response) => response.text())
+    .then((result) => {
+      const wordArr = result.split("\n");
+      todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)];
+      wordSet = new Set(wordArr);
+    });
+  return { wordSet, todaysWord };
+};
