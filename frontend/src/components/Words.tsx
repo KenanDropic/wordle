e7a236a363fd3dda9dded8bce5652662fb6a7e9e
Kenan Dropić <dropic5@hotmail.com>
2022-06-29 12:48:52 +0200

    update words & store

--- frontend/src/components/Words.tsx
+++ frontend/src/components/Words.tsx
@@ -1,7 +1,3 @@
-import words from "../words-data.txt";
-import axios from "axios";
-console.log(words);
-
 export const boardDefault: string[][] = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
@@ -10,16 +6,3 @@ export const boardDefault: string[][] = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
 ];
-
-export const generateWordSet = async () => {
-  let wordSet;
-  let todaysWord;
-  await fetch(words)
-    .then((response) => response.text())
-    .then((result) => {
-      const wordArr = result.split("\n");
-      todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)];
-      wordSet = new Set(wordArr);
-    });
-  return { wordSet, todaysWord };
-};
--- frontend/src/store.tsx
+++ frontend/src/store.tsx
@@ -1,11 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "@reduxjs/toolkit";
 import globalReducer from "./features/globalSlice";
+import { enableMapSet } from "immer";
 
+enableMapSet();
 const rootReducer = combineReducers({});
 
 export const store = configureStore({
   reducer: { global: globalReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["global/setWordSet", "words/getRandom/fulfilled"],
+        ignoredPaths: ["global.words.wordSet"],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
