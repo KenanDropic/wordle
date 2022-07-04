5dc525ce22cb1cd2013af22150ce548c5ce8a9d0
Kenan Dropić <dropic5@hotmail.com>
2022-07-04 22:26:55 +0200

    check if origin allowed

---
+++ backend/middleware/credentials.ts
@@ -0,0 +1,15 @@
+import allowedOrigins from "../config/allowedOrigins";
+import { NextFunction, Request, Response } from "express";
+
+const credentials = (req: Request, res: any, next: NextFunction) => {
+  const origin = req.headers.origin;
+  if (origin !== undefined) {
+    if (allowedOrigins.includes(origin)) {
+      res.header("Access-Control-Allow-Credentials", true);
+    }
+  }
+  console.log("Origin:", origin);
+  next();
+};
+
+export default credentials;
