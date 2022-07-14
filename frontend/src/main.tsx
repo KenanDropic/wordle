561a58a2fe9a052182af8171a18d79a759867de3
Kenan Dropić <dropic5@hotmail.com>
2022-07-14 11:23:40 +0200

    convert css to sass

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import "./index.css";
+// import "./index.css";
+import "./sass/main.scss";
 import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
