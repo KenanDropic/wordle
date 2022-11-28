33d7c5ee90350ba294a6276523c00a9596c2d9c0
Kenan Dropić <dropic5@hotmail.com>
2022-11-28 19:43:00 +0100

    generate manifest.json

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
-import Board from "./components/Board";
-import Keyboard from "./components/Keyboard";
-import Nav from "./components/Nav";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { CookiesProvider } from "react-cookie";
+import 'vite/modulepreload-polyfill'
 
 const App: React.FC = () => {
   return (
