173d7f9980205abf8726a20dfc800d750ac72c6b
Kenan Dropić <dropic5@hotmail.com>
2022-11-29 15:02:12 +0100

    change baseURL for production mode

--- frontend/src/app/api/apiSlice.tsx
+++ frontend/src/app/api/apiSlice.tsx
@@ -15,7 +15,8 @@ const mutex = new Mutex();
 
 const baseQuery: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> =
   fetchBaseQuery({
-    baseUrl: "http://127.0.0.1:5000/api/v1",
+    // baseUrl: "http://127.0.0.1:5000/api/v1", baseUrl for development
+    baseUrl: "https://wordle-f32h.onrender.com/api/v1/",
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
       const accessToken: string = (getState() as any).auth.access_token;
