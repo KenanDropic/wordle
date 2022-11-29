5de1ffc760a497649023f0ac553e81bd692ff9a0
Kenan Dropić <dropic5@hotmail.com>
2022-11-29 07:40:39 +0100

    cors

--- backend/config/allowedOrigins.ts
+++ backend/config/allowedOrigins.ts
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5000",
   "http://127.0.0.1:5000/api/v1/auth/login",
+  "https://wordle-f32h.onrender.com",
 ];
 
 export default allowedOrigins;
