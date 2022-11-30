b87a56f6c50e2b56ce394257b4dbb02a811a46f7
Kenan Dropić <dropic5@hotmail.com>
2022-11-30 09:25:42 +0100

    modify responsive design

--- frontend/src/components/GameEnd.tsx
+++ frontend/src/components/GameEnd.tsx
@@ -8,7 +8,7 @@ const GameEnd: React.FC = () => {
     gameOver: { isWon },
   } = useAppSelector((state) => state.global);
   return (
-    <div className="gameOver">
+    <div style={{ marginTop: "25%" }}>
       <h3>{isWon ? "You Correctly guessed" : "You failed"}</h3>
       <h1>Correct: {todaysWord}</h1>
       {isWon && <h3>{`You guessed in ${currentAttempt.attempt} attempts`}</h3>}
